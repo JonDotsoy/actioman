c11668840d1d9fee7ea6fcd084727bbdcfc73177
Jonathan Delgado <hi@jon.soy>
2025-01-18 00:00:41 -0300

    feat(actions): add new actions and tests
    This commit introduces a new set of actions and their corresponding tests. The key changes are:
    
    1. Added a new `Actions` class that manages a collection of actions, allowing for easy calling and type-safe handling of action inputs and outputs.
    2. Implemented the `defineAction` function to create new actions with optional input and output schemas defined using the `zod` library.
    3. Added several test cases to ensure the correct behavior of the `Actions` class and the defined actions.
    4. Introduced utility types to infer the input and output types of the actions, enabling type-safe usage of the `Actions` class.
    
    These changes improve the overall structure and type-safety of the actions system, making it easier to manage and extend the application's functionality.

---
+++ src/actions/actions.spec.ts
@@ -0,0 +1,67 @@
+import { describe, expect, it } from "bun:test";
+import { z } from "zod";
+import { expectTypeOf } from "expect-type";
+import { Actions, defineAction } from "./actions";
+
+describe("", () => {
+  it("", () => {
+    const actions = new Actions({
+      foo: defineAction({
+        handler: async () => "ok",
+      }),
+    });
+  });
+
+  it("", async () => {
+    const actions = new Actions({
+      foo: defineAction({
+        handler: async () => "ok",
+      }),
+      taz: defineAction({
+        handler: async () => "ok",
+      }),
+    });
+
+    expect(await actions.call("foo", {})).toEqual("ok foo");
+  });
+
+  it("", async () => {
+    const actions = new Actions({
+      hi: defineAction({
+        input: z.object({
+          name: z.string(),
+        }),
+        output: z.string(),
+        handler: async ({ name }) => `hello ${name}`,
+      }),
+    });
+
+    expect(await actions.call("hi", { name: "juan" })).toEqual("hello juan");
+  });
+
+  it("", async () => {
+    const actions = new Actions({
+      hi: defineAction({
+        input: z.object({
+          name: z.string(),
+        }),
+        output: z.string(),
+        handler: async ({ name }) => `hello ${name}`,
+      }),
+    });
+
+    type InputFromHiAction<T> = T extends (name: "hi", input: infer r) => any
+      ? r
+      : never;
+    type ExtractResponseType<T> = T extends (name: "hi", input: any) => infer r
+      ? r
+      : never;
+    type callActionInput = InputFromHiAction<typeof actions.call>;
+    type extractedResponseType = Awaited<
+      ExtractResponseType<typeof actions.call>
+    >;
+
+    expectTypeOf<callActionInput>().toEqualTypeOf<{ name: string }>;
+    expectTypeOf<extractedResponseType>().toEqualTypeOf<string>;
+  });
+});
