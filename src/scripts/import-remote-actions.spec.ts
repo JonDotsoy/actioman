0dd80ab04f5e3e2eb1f970921e31d4561efad70a
Jonathan Delgado <hi@jon.soy>
2025-01-21 20:41:27 -0300

    feat(import-remote-actions): Add functionality to import remote actions
    This commit adds a new script `import-remote-actions.ts` that provides a
    function to import remote actions from a URL and integrate them into the
    project's actions system. The key changes include:
    
    - Implement a function to recursively search for the `actioman` node
      module path starting from the current working directory.
    - Provide a function to find the `share-actions.js` module within the
      `actioman` node module.
    - Implement a function to normalize the action name and write the
      imported action to the `share-actions.js` module.
    - Add a test suite in `import-remote-actions.spec.ts` to verify the
      functionality of the `importRemoteActions` function.

---
+++ src/scripts/import-remote-actions.spec.ts
@@ -0,0 +1,45 @@
+import { afterEach, beforeAll, beforeEach, describe, it } from "bun:test";
+import { $ } from "../shell/shell";
+import { importRemoteActions } from "./import-remote-actions";
+import { HTTPLister } from "../http-router/http-listener";
+import { defineAction } from "../actions/actions";
+import { set, z } from "zod";
+
+describe.only("import-remote-actions", () => {
+  const cleanupTasks = new Set<() => any>();
+
+  afterEach(async () => {
+    for (const cleanupTask of cleanupTasks) await cleanupTask();
+    cleanupTasks.clear();
+  });
+
+  beforeAll(async () => {
+    await $`
+      rm -rf __tests__/tmp/
+      echo "*" > __tests__/.gitignore
+      mkdir -p __tests__/tmp/
+      mkdir -p __tests__/tmp/node_modules/actioman/lib/esm/share-actions/
+      mkdir -p __tests__/tmp/node_modules/actioman/lib/esm/actions-target/
+      echo 'export * from "../../../../../../../../actions-target/actions-target.js";' > __tests__/tmp/node_modules/actioman/lib/esm/actions-target/actions-target.js
+      echo "export const shareActions = {};" > __tests__/tmp/node_modules/actioman/lib/esm/share-actions/share-actions.js
+    `.cwd(new URL("./", import.meta.url).pathname);
+  });
+
+  it("", async () => {
+    const httpLister = HTTPLister.fromModule({
+      hi: defineAction({
+        input: z.object({ name: z.string() }),
+        handler: async ({ name }) => `Hello, ${name}!`,
+      }),
+    });
+    cleanupTasks.add(() => httpLister.close());
+
+    const server = await httpLister.listen(0);
+
+    await importRemoteActions(
+      server,
+      "1.- my actions 1",
+      new URL("./__tests__/tmp/", import.meta.url).pathname,
+    );
+  });
+});
