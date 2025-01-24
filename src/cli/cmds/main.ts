a4a934594bc1c91fd48cf9edadb61489868fa9bc
Jonathan Delgado <hi@jon.soy>
2025-01-24 00:25:31 -0300

    feat(cli): add install command to prepare remotes
    This commit adds a new `install` command to the CLI. The `install` command
    is used to prepare remotes for use by the application. This is an important
    feature as it allows users to easily set up the necessary dependencies for
    the application to function correctly.
    
    The changes include:
    
    - Adding a new `install` command to the `main.ts` file, which calls the
      `install` function when the `install` option is provided.
    - Adding a new `install.js` file to handle the logic for the `install`
      command.
    
    These changes will improve the user experience by providing a simple way
    for users to set up the application and its dependencies.

--- src/cli/cmds/main.ts
+++ src/cli/cmds/main.ts
@@ -10,11 +10,13 @@ import {
 } from "@jondotsoy/flags";
 import { serve } from "./serve.js";
 import { add } from "./add.js";
+import { install } from "./install.js";
 
 export const main = async (args: string[]) => {
   type Options = {
     help: boolean;
     serve: string[];
+    install: string[];
     add: string[];
   };
   const rules: Rule<Options>[] = [
@@ -23,6 +25,9 @@ export const main = async (args: string[]) => {
     }),
     rule(command("add"), restArgumentsAt("add"), {
       description: "Start server",
+    }),
+    rule(command("install"), restArgumentsAt("install"), {
+      description: "Prepare remotes for use",
     }),
     rule(flag("-h", "--help"), isBooleanAt("help"), {
       description: "Show help",
@@ -33,6 +38,7 @@ export const main = async (args: string[]) => {
   if (options.help) return console.log(makeHelpMessage("actioman", rules));
   if (options.add) return await add(options.add);
   if (options.serve) return await serve(options.serve);
+  if (options.install) return await install(options.install);
 
   return console.log(makeHelpMessage("actioman", rules));
 };
