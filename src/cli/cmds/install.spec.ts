a403a067fced461b5f552b20b0e981a435b5585b
Jonathan Delgado <hi@jon.soy>
2025-01-24 00:24:26 -0300

    tests(cli): add install command tests
    The changes add new test cases for the `install` command in the CLI. The
    first test case verifies that the `install` command can be executed
    without errors. The second test case adds a remote action and then
    installs it, ensuring that the remote action can be used.
    
    These changes are important to ensure the reliability and functionality
    of the `install` command in the CLI.

---
+++ src/cli/cmds/install.spec.ts
@@ -0,0 +1,46 @@
+import { describe, it } from "bun:test";
+import { PrepareWorkspace } from "../utils/prepare-workspace.js";
+import { CleanupTasks } from "@jondotsoy/utils-js/cleanuptasks";
+import { HTTPLister } from "../../http-router/http-listener.js";
+import { defineAction } from "../../actions/actions.js";
+import { z } from "zod";
+
+describe("install", async () => {
+  it(
+    "should install without errors",
+    async () => {
+      const { $work } = await PrepareWorkspace.setup(import.meta.url);
+      await $work`
+      npx actioman install
+    `;
+    },
+    { timeout: 60_000 },
+  );
+});
+
+describe("install", async () => {
+  it(
+    "should install remote actions and use them",
+    async () => {
+      const { $work } = await PrepareWorkspace.setup(import.meta.url);
+      await using cleanupTasks = new CleanupTasks();
+      cleanupTasks.add(() => httpLocation.close());
+
+      const httpLocation = HTTPLister.fromModule({
+        hi: defineAction({
+          input: z.object({ name: z.string() }),
+          output: z.string(),
+          handler: async ({ name }) => `hello ${name}!`,
+        }),
+      });
+      const serviceUrl = await httpLocation.listen();
+
+      await $work`npx actioman add "my action" ${serviceUrl.toString()}`;
+
+      await $work`
+      npx actioman install
+    `;
+    },
+    { timeout: 60_000 },
+  );
+});
