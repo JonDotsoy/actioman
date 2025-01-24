54187cbf673653c02c73143967fa7b0455bea647
Jonathan Delgado <hi@jon.soy>
2025-01-24 00:24:33 -0300

    tests: Refactor add.spec.ts to use PrepareWorkspace utility
    This change refactors the add.spec.ts file to use the PrepareWorkspace utility
    instead of the custom setup code. This simplifies the test setup and makes it
    easier to maintain. The most important changes are:
    
    - Removed the custom beforeEach setup code and replaced it with the
      PrepareWorkspace utility
    - Removed the custom shell script logic and replaced it with the
      PrepareWorkspace utility
    - Focused the test on the core functionality of the add command

--- src/cli/cmds/add.spec.ts
+++ src/cli/cmds/add.spec.ts
@@ -1,61 +1,13 @@
-import { afterEach, beforeAll, beforeEach, describe, it } from "bun:test";
-import { add } from "./add.js";
+import { describe, it } from "bun:test";
 import { HTTPLister } from "../../http-router/http-listener.js";
 import { defineAction } from "../../actions/actions";
 import { z } from "zod";
-import { $ } from "../../shell/shell.js";
 import * as fs from "fs/promises";
 import { CleanupTasks } from "@jondotsoy/utils-js/cleanuptasks";
+import { PrepareWorkspace } from "../utils/prepare-workspace.js";
 
-beforeEach(async () => {
-  await $`
-    TEST_DIR=__tests__/sample_project/
-    rm -rf $TEST_DIR
-    mkdir -p $TEST_DIR
-    echo "*" > "__tests__/.gitignore"
-  `.cwd(new URL("./", import.meta.url).pathname);
-});
-
-describe("add", () => {
-  const $work = $.cwd(
-    new URL("./__tests__/sample_project/", import.meta.url).pathname,
-  );
-
-  beforeEach(async () => {
-    await $`
-      current_hash=$(cat /tmp/caches/t923051323_1.hash)
-      hash=$(find src/ -type f -not -name  "*.spec.*" | sort  | xargs -I {} cat {} | shasum | awk '{{print $1}}')
-      echo $hash > /tmp/caches/t923051323_1.hash
-      if [ "$current_hash" = "$hash" ]
-      then
-        rm -f  /tmp/caches/t923051323_1_actioman_module.changed
-        exit 0
-      fi
-      npm pack
-      tarfile=$(realpath $(cat package.json | jq -r '"" + .name + "-" + .version + ".tgz"'))
-      echo tarfile=$tarfile
-      mv $tarfile /tmp/caches/t923051323_1_actioman_module.tgz
-      echo "true" > /tmp/caches/t923051323_1_actioman_module.changed
-    `;
-
-    await $work`
-      mkdir -p /tmp/caches
-      if [ -f /tmp/caches/t923051323_1.tgz ]
-      then
-        tar -xzf /tmp/caches/t923051323_1.tgz
-      fi
-      if [ ! -f "package.json" ]
-      then
-        npm init -y
-        npm pkg set type=module
-      fi
-      if [ -f /tmp/caches/t923051323_1_actioman_module.changed ]
-      then
-        npm add /tmp/caches/t923051323_1_actioman_module.tgz
-      fi
-      tar -czf /tmp/caches/t923051323_1.tgz .
-    `;
-  });
+describe("add", async () => {
+  const { $work } = await PrepareWorkspace.setup(import.meta.url);
 
   it("should add remote actions and use them", async () => {
     await using cleanupTasks = new CleanupTasks();
