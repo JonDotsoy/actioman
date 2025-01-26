715ac0a234c6fcbd33e2e574b23bc9d65c233b65
Jonathan Delgado <hi@jon.soy>
2025-01-26 18:53:02 -0300

    feat: Implement cache-based workspace setup
    This commit introduces a cache-based workspace setup mechanism to
    improve the performance of the test suite. The key changes are:
    
    - Implement a `CacheDir` class to manage the cache directory and
      handle caching of the workspace directory.
    - Use the `CacheDir` class in the `PrepareWorkspace` class to set up
      the workspace directory, caching the workspace if it has not changed.
    - Introduce utility functions to calculate the SHA256 hash of the
      workspace directory, which is used as the cache key.
    - Refactor the existing workspace setup logic to leverage the new
      cache-based approach.
    
    The goal of these changes is to reduce the time required to set up the
    test workspace by caching the workspace directory and only updating it
    when the source files have changed.

--- src/cli/utils/prepare-workspace.ts
+++ src/cli/utils/prepare-workspace.ts
@@ -1,67 +1,180 @@
-import { beforeEach } from "bun:test";
+import * as fs from "fs/promises";
+import { existsSync } from "fs";
+import * as os from "os";
 import { $ } from "../../shell/shell.js";
 
-const TMP_FILE_NAME =
-  "0x06faf929b5dbc136c97d5e550a59a21a111f000021e81a62bf065030c842ab9f3";
+type setupOptions = {
+  name?: string;
+  showLogs?: boolean;
+};
 
-export class PrepareWorkspace {
-  static async setup(metaUrl: string) {
-    await $`
-      set -e
-      TEST_DIR=__tests__/sample_project/
-      rm -rf $TEST_DIR
-      mkdir -p $TEST_DIR
-      echo "*" > "__tests__/.gitignore"
-    `.cwd(new URL("./", metaUrl).pathname);
+const sha256 = async (body: string) => {
+  const digest = new Uint8Array(
+    await crypto.subtle.digest("sha256", new TextEncoder().encode(body)),
+  );
 
-    const $work = $.cwd(
-      new URL("./__tests__/sample_project/", metaUrl).pathname,
+  return Array.from(digest, (e) => e.toString(16).padStart(2, "0")).join("");
+};
+
+class CacheDir {
+  private constructor(
+    readonly cacheLocation: URL,
+    readonly location: URL,
+  ) {}
+
+  async setup() {
+    const tarFile = new URL(
+      `${await sha256(this.location.toString())}.tgz`,
+      this.cacheLocation,
+    );
+    await fs.mkdir(this.cacheLocation, { recursive: true });
+
+    if (existsSync(tarFile)) {
+      await $`
+        tar -xzf $TARFILE -C $LOCATION
+      `.appendEnvs({
+        TARFILE: tarFile.pathname,
+        LOCATION: this.location.pathname,
+      });
+    }
+
+    return this;
+  }
+
+  async [Symbol.asyncDispose]() {
+    const tarFile = new URL(
+      `${await sha256(this.location.toString())}.tgz`,
+      this.cacheLocation,
     );
 
     await $`
-      set -e
-      current_hash=""
-      mkdir -p /tmp/__tests/caches/
-      if [ -f "/tmp/__tests/caches/${TMP_FILE_NAME}.hash" ]
-      then
-        current_hash=$(cat /tmp/__tests/caches/${TMP_FILE_NAME}.hash)
-      fi
-      hash=$(find src/ -type f -not -name  "*.spec.*" | sort  | xargs -I {} cat {} | shasum | awk '{{print $1}}')
-      echo $hash > /tmp/__tests/caches/${TMP_FILE_NAME}.hash
-      if [ "$current_hash" = "$hash" ]
-      then
-        rm -f  /tmp/__tests/caches/${TMP_FILE_NAME}_actioman_module.changed
-        exit 0
-      fi
-      npm pack
-      tarfile=$(realpath $(cat package.json | jq -r '"" + .name + "-" + .version + ".tgz"'))
-      echo tarfile=$tarfile
-      mv $tarfile /tmp/__tests/caches/${TMP_FILE_NAME}_actioman_module.tgz
-      echo "true" > /tmp/__tests/caches/${TMP_FILE_NAME}_actioman_module.changed
-    `;
+      tar -czf $TARFILE -C $LOCATION .
+    `.appendEnvs({
+      TARFILE: tarFile.pathname,
+      LOCATION: this.location.pathname,
+    });
+  }
 
-    await $work`
-      set -e
-      mkdir -p /tmp/caches
-      if [ -f /tmp/__tests/caches/${TMP_FILE_NAME}.tgz ]
-      then
-        tar -xzf /tmp/__tests/caches/${TMP_FILE_NAME}.tgz || true
-      fi
-      if [ ! -f "package.json" ]
-      then
-        npm init -y
-        npm pkg set "type=module"
-      fi
-      if [ -f /tmp/__tests/caches/${TMP_FILE_NAME}_actioman_module.changed ]
-      then
-        npm add /tmp/__tests/caches/${TMP_FILE_NAME}_actioman_module.tgz
-      fi
-      tar -czf /tmp/__tests/caches/${TMP_FILE_NAME}.tgz . || true
-    `;
+  static async setup(cacheLocation: URL, location: URL) {
+    return new CacheDir(cacheLocation, location).setup();
+  }
+}
 
-    return {
-      $,
-      $work,
+export class PrepareWorkspace {
+  cacheDir = new URL(
+    "__prepare-workspace/caches/",
+    new URL(`${os.tmpdir()}/`, "file://"),
+  );
+  cachePackagesDir = new URL("./packages/", this.cacheDir);
+  cacheWorkspacesDir = new URL("./workspaces/", this.cacheDir);
+  workspacesDir = new URL("./workspaces/", this.cacheDir);
+  private _workspaceDir?: URL | undefined;
+  packageDir = new URL("../../../", import.meta.url);
+  _hash?: string;
+  cache?: CacheDir;
+
+  private constructor() {}
+
+  getHash() {
+    if (!this._hash) throw new Error("Hash not set");
+    return this._hash;
+  }
+
+  setHash(hash: string) {
+    this._hash = hash;
+  }
+
+  public get workspaceDir(): URL {
+    if (!this._workspaceDir) throw new Error("Workspace not set");
+    return this._workspaceDir;
+  }
+  public set workspaceDir(value: URL) {
+    this._workspaceDir = value;
+  }
+
+  $ = (arr: TemplateStringsArray, ...args: string[]) =>
+    $(arr, ...args).cwd(this.workspaceDir.pathname);
+
+  async describePackage() {
+    const filter = (relativePath: string): boolean => {
+      if (relativePath.startsWith(".git/")) return false;
+      if (relativePath.startsWith(".tmp/")) return false;
+      if (relativePath.startsWith("lib/")) return false;
+      if (relativePath.startsWith("node_modules/")) return false;
+      if (/spec\.\w+$/.test(relativePath)) return false;
+      if (/\/_+\w+_+\//.test(relativePath)) return false;
+      if (!/\.(ts|json)+$/.test(relativePath)) return false;
+      return true;
     };
+
+    const files = await fs.readdir(this.packageDir, { recursive: true });
+
+    return files.filter((f) => filter(f)).sort();
+  }
+
+  async hashPackage() {
+    const buff: number[] = [];
+    for (const e of await this.describePackage()) {
+      const path = new URL(e, this.packageDir);
+      buff.push(...new Uint8Array(await fs.readFile(path)));
+    }
+    const hash = new Uint8Array(
+      await crypto.subtle.digest("sha-256", new Uint8Array(buff)),
+    );
+    return `0x${Array.from(hash, (e) => e.toString(16).padStart(2, "0")).join("")}`;
+  }
+
+  packagePath() {
+    return new URL(`${this.getHash()}.tgz`, this.cachePackagesDir);
+  }
+
+  async copyPackage(tarfile: string) {
+    await fs.copyFile(tarfile, this.packagePath());
+  }
+
+  async setup(options: setupOptions = {}) {
+    const showLogs = options.showLogs ?? false;
+    const workspaceName = options.name ?? "default";
+    this.workspaceDir = new URL(`${workspaceName}/`, this.workspacesDir);
+    await fs.mkdir(this.cacheDir, { recursive: true });
+    await fs.mkdir(this.cacheWorkspacesDir, { recursive: true });
+    await fs.mkdir(this.cachePackagesDir, { recursive: true });
+    await fs.mkdir(this.workspaceDir, { recursive: true });
+    await fs.rm(this.workspaceDir, { force: true });
+    await using cache = await CacheDir.setup(
+      this.cacheWorkspacesDir,
+      this.workspaceDir,
+    );
+
+    this.cache = cache;
+
+    this._hash = await this.hashPackage();
+    const packagePath = await this.packagePath();
+    if (!existsSync(packagePath)) {
+      await $`npm pack`.cwd(this.packageDir.pathname).quiet(!showLogs);
+      const tarfile =
+        await $`realpath $(cat package.json | jq -r '"" + .name + "-" + .version + ".tgz"')`
+          .cwd(this.packageDir.pathname)
+          .text();
+      await this.copyPackage(tarfile.trim());
+    }
+
+    await this.$`
+      asdf local nodejs 23.6.1
+      npm init -f
+      npm add $PACKAGE_PATH
+    `
+      .appendEnvs({
+        PACKAGE_PATH: this.packagePath().pathname,
+      })
+      .quiet(!showLogs);
+
+    return this;
+  }
+
+  async run() {}
+
+  static async setup(options: setupOptions = {}) {
+    return new PrepareWorkspace().setup(options);
   }
 }
