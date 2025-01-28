7d45f113d7c84e2de492ac6f730dbae4ddec1703
Jonathan Delgado <hi@jon.soy>
2025-01-27 22:22:43 -0300

    tests: generate bootstrap script for Bun server
    This commit introduces a new feature to generate a bootstrap script for a Bun server. The changes include:
    
    - Added a new test case to verify the generated bootstrap script
    - Implemented the `makeServerScript` function to generate the bootstrap script
    - Created a temporary workspace directory to test the script generation
    - Wrote the generated bootstrap script to a snapshot file for verification
    
    The main purpose of these changes is to provide a utility function that can generate a boilerplate script to start a Bun server, which includes setting up the HTTP listener and logging the server URL.

--- src/scripts/make-server-script.spec.ts
+++ src/scripts/make-server-script.spec.ts
@@ -1,12 +1,40 @@
-import { describe, it } from "bun:test";
+import { describe, expect, it } from "bun:test";
 import { $ } from "../shell/shell";
 import { makeServerScript } from "./make-server-script.js";
-import { PrepareWorkspace } from "../cli/utils/prepare-workspace.js";
+import * as fs from "fs/promises";
 
-describe("", () => {
-  it.skip("", async () => {
-    const {} = await PrepareWorkspace.setup();
+describe("makeServerScript", () => {
+  it("generates bootstrap script", async () => {
+    const WORKSPACE_PATH = "./__tests__/__workspaces__/make-server-script/";
 
-    await makeServerScript("");
+    await $`
+      rm -rf $WORKSPACE_PATH
+      mkdir -p $WORKSPACE_PATH
+      echo "*" > $WORKSPACE_PATH/.gitignore
+    `
+      .cwd(new URL("./", import.meta.url).pathname)
+      .appendEnvs({ WORKSPACE_PATH });
+
+    await $`
+      mkdir -p node_modules/actioman/lib/esm/http-router/
+      touch node_modules/actioman/lib/esm/http-router/http-listener.js
+      touch actions.js
+    `.cwd(new URL(WORKSPACE_PATH, import.meta.url).pathname);
+
+    const actionsPathRaw = await $`realpath actions.js`
+      .cwd(new URL(WORKSPACE_PATH, import.meta.url).pathname)
+      .text();
+    const actionsPath = actionsPathRaw.trim();
+
+    const res = await makeServerScript(
+      new URL(WORKSPACE_PATH, import.meta.url).pathname,
+      actionsPath,
+    );
+
+    expect(res.bootstrapLocation).toBeTypeOf("string");
+    expect(res.httpListenerLocation).toBeTypeOf("string");
+    expect(
+      await fs.readFile(new URL(res.bootstrapLocation), "utf-8"),
+    ).toMatchSnapshot();
   });
 });
