879284372f0831f5d79deaeec89a9a41a2503a49
Jonathan Delgado <hi@jon.soy>
2025-01-28 22:47:37 -0300

    tests: add serve command test
    This commit adds a test for the `serve` command in the CLI. The test
    sets up a workspace, creates a simple JavaScript module, and starts a
    server using the `actioman serve` command. It then verifies that the
    server returns a 404 response when an unknown resource is requested.
    This ensures the basic functionality of the `serve` command.

---
+++ src/cli/cmds/serve.spec.ts
@@ -0,0 +1,43 @@
+import { describe, expect, it } from "bun:test";
+import { PrepareWorkspace } from "../utils/prepare-workspace";
+import { CleanupTasks } from "@jondotsoy/utils-js/cleanuptasks";
+
+describe("serve", () => {
+  it(
+    "should start a server and return 404 if the requested resource is not found",
+    async () => {
+      const cleanupTasks = new CleanupTasks();
+      cleanupTasks.add(() => subProcess.close());
+      const { $ } = await PrepareWorkspace.setup();
+
+      await $`
+        npm pkg set type=module
+        echo "export const hi = () => 'hola';" > app.js
+      `;
+
+      const subProcess = $`
+        npx actioman serve app.js
+      `.background();
+
+      const serviceUrl = await new Promise<string>((resolve) => {
+        subProcess.stdout.pipeTo(
+          new WritableStream({
+            write: (buff) => {
+              const url = /Server running at (?<url>.+)\n/.exec(
+                new TextDecoder().decode(buff),
+              )?.groups?.url;
+              if (url) resolve(url);
+            },
+          }),
+        );
+      });
+
+      const res = await fetch(new URL(serviceUrl));
+
+      expect(res.status).toEqual(404);
+    },
+    {
+      timeout: 60_000,
+    },
+  );
+});
