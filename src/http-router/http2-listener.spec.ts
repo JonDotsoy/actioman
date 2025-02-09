d722b28a1fb38e3658f1fbc5e6449a3af7248fb8
Jonathan Delgado <hi@jon.soy>
2025-02-08 21:58:40 -0300

    feat(http-router): add HTTP2 listener with tests
    The changes in this commit add a new `HTTP2Lister` class that creates an HTTP2 server and integrates it with the existing `HTTPRouter` class. This allows the application to serve HTTP2 requests in addition to HTTP1.1 requests.
    
    The key changes include:
    
    - Adding a new `HTTP2Lister` class that creates an HTTP2 server and handles incoming requests by forwarding them to the `HTTPRouter`.
    - Adding a new `requestHttp2ToRequest` utility function that converts an `http2.Http2ServerRequest` object to a standard `Request` object.
    - Adding a new `sanatizeHostname` utility function that normalizes the hostname for the HTTP2 server.
    - Adding a new test suite for the `HTTP2Lister` class to ensure it correctly handles HTTP2 requests.
    
    These changes allow the application to serve both HTTP1.1 and HTTP2 requests, providing a more modern and efficient way to handle incoming requests.

---
+++ src/http-router/http2-listener.spec.ts
@@ -0,0 +1,48 @@
+import { CleanupTasks } from "@jondotsoy/utils-js/cleanuptasks";
+import { describe, expect, it } from "bun:test";
+import { HTTP2Lister } from "./http2-listener.js";
+import * as http2 from "http2";
+import { DEFAULT_CERT } from "./DEFAULT_CERT.js";
+
+describe("async HTTP2Lister", () => {
+  it("should return 200 for /__actions", async () => {
+    await using cleanupTasks = new CleanupTasks();
+
+    const http2Lister = HTTP2Lister.fromModule({
+      hi: () => "ok",
+    });
+    cleanupTasks.add(() => http2Lister.close());
+
+    const url = await http2Lister.listen();
+
+    const client = http2.connect(url, { ca: DEFAULT_CERT });
+    cleanupTasks.add(() => client.close());
+
+    const { status, body } = await new Promise<{ body: any; status: number }>(
+      (resolve, reject) => {
+        const data: number[] = [];
+        let status: any;
+
+        client
+          .request({
+            [http2.constants.HTTP2_HEADER_PATH]: "/__actions",
+            [http2.constants.HTTP2_HEADER_METHOD]: "GET",
+          })
+          .addListener("error", (err) => reject(err))
+          .addListener("response", (headers) => {
+            status = headers[http2.constants.HTTP2_HEADER_STATUS];
+          })
+          .addListener("data", (d) => data.push(...d))
+          .addListener("close", () => {
+            resolve({
+              status,
+              body: new TextDecoder().decode(new Uint8Array(data)),
+            });
+          });
+      },
+    );
+
+    expect(status).toEqual(200);
+    expect(body).toMatchSnapshot();
+  });
+});
