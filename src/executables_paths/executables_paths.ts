ee0049a34b5d4b1f586c4804d5852447aff0ff82
Jonathan Delgado <hi@jon.soy>
2025-02-10 18:02:34 -0300

    feat(paths): Add executable paths for configs and listeners
    The changes in this commit add new export variables for the paths of various
    configuration and listener files. This allows for centralized management of
    these paths, making it easier to reference and update them throughout the
    application.
    
    The key changes include:
    
    - Adding `HTTP_LISTENER_PATH`, `HTTP2_LISTENER_PATH`, `FACTORY_PATH`, and
      `CONFIG_PATH` exports to provide the paths for the corresponding files.
    - Creating an `executablesPaths` object to consolidate all the executable
      paths in a single location.
    
    These changes improve the maintainability and flexibility of the application
    by providing a centralized way to manage the paths to various configuration
    and listener files.

---
+++ src/executables_paths/executables_paths.ts
@@ -0,0 +1,12 @@
+import { CONFIG_PATH } from "../configs/configs_path.js";
+import { FACTORY_PATH } from "../configs/modules/factory_path.js";
+import { HTTP_LISTENER_PATH } from "../http-router/http-listener-path.js";
+import { HTTP2_LISTENER_PATH } from "../http-router/http2-listener-path.js";
+
+/** Display paths executables */
+export const executablesPaths = {
+  HTTP_LISTENER_PATH,
+  HTTP2_LISTENER_PATH,
+  FACTORY_PATH,
+  CONFIG_PATH,
+};
---
+++ src/http-router/http2-listener-path.ts
@@ -0,0 +1,4 @@
+export const HTTP2_LISTENER_PATH = new URL(
+  "./http2-listener.js",
+  import.meta.url,
+);
