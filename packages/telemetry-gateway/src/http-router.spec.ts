99cbfd62371cbac4cd20d41bb881dd8e655ee518
Jonathan Delgado <hi@jon.soy>
2025-03-04 19:20:00 -0300

    feat(telemetry-gateway): Add default series register to http-router.spec.ts
    The changes in this commit add the `defaultRegistry` import from `./metrics_control/registers/default_serie_register` to the `http-router.spec.ts` file. This is necessary to ensure that the default series register is available for the tests in the `HTTPRouter` suite.

--- packages/telemetry-gateway/src/http-router.spec.ts
+++ packages/telemetry-gateway/src/http-router.spec.ts
@@ -1,6 +1,7 @@
 import { afterEach, beforeAll, describe, expect, it } from "bun:test";
 import { HTTPRouter } from "./http-router";
 import client from "prom-client";
+import { defaultRegistry } from "./metrics_control/registers/default_serie_register";
 
 describe("HTTPRouter", () => {
   afterEach(() => {
@@ -26,47 +27,5 @@ describe("HTTPRouter", () => {
     );
 
     expect(res.status).toEqual(200);
-  });
-
-  it("should collect metrics correctly", async () => {
-    const router = new HTTPRouter();
-
-    const events: any[] = [
-      {
-        type: "command_execution",
-        actioman_version: "1.0.0",
-        command: "serve",
-        duration_ms: 120,
-      },
-      {
-        type: "command_execution",
-        actioman_version: "1.1.0",
-        command: "serve",
-        duration_ms: 430,
-      },
-      {
-        type: "command_execution",
-        actioman_version: "1.0.0",
-        command: "serve",
-        plugins_active: ["foo", "taz"],
-        duration_ms: 212,
-      },
-    ];
-
-    for (const event of events) {
-      await router.router.fetch(
-        new Request("http://localhost:3000/collect", {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(event),
-        }),
-      );
-    }
-
-    console.log(await client.register.metrics());
-
-    expect(await client.register.getMetricsAsJSON()).toMatchSnapshot();
   });
 });
