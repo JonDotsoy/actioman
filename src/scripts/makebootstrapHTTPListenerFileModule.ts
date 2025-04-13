8e0d1fcd1e5eee123d68dc8530a3653a400db10c
Jonathan Delgado <hi@jon.soy>
2025-04-12 20:05:10 -0400

    refactor: simplify makebootstrapHTTPListenerFileModule to return getBootstrapFilesLocation

--- src/scripts/makebootstrapHTTPListenerFileModule.ts
+++ src/scripts/makebootstrapHTTPListenerFileModule.ts
@@ -1,9 +1,10 @@
-import { findActiomanNodeModulesPaths } from "./findNodeModulesPaths.js";
+import { getBootstrapFilesLocation } from "../bootstrap-files-location/bootstrap-files-location.js";
+// import { findActiomanNodeModulesPaths } from "./findNodeModulesPaths.js";
 
 export async function makebootstrapHTTPListenerFileModule(cwd: URL) {
-  const actiomanNodeModulePath: URL | null =
-    (await findActiomanNodeModulesPaths(cwd).next()).value ?? null;
-  if (!actiomanNodeModulePath) throw new Error("actioman is not installed");
-  const actionsFolder = new URL("./.cache/serve.js", actiomanNodeModulePath);
-  return actionsFolder;
+  // const actiomanNodeModulePath: URL | null =
+  //   (await findActiomanNodeModulesPaths(cwd).next()).value ?? null;
+  // if (!actiomanNodeModulePath) throw new Error("actioman is not installed");
+  // const actionsFolder = new URL("./.cache/serve.js", actiomanNodeModulePath);
+  return getBootstrapFilesLocation();
 }
