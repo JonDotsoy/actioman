e3864bbd84cec02a26e0d918f207b5b0270ff864
Jonathan Delgado <hi@jon.soy>
2025-04-12 23:24:20 -0400

    refactor: simplify makebootstrapHTTPListenerFileModule by removing commented code

--- src/scripts/makebootstrapHTTPListenerFileModule.ts
+++ src/scripts/makebootstrapHTTPListenerFileModule.ts
@@ -1,10 +1,4 @@
 import { getBootstrapFilesLocation } from "../bootstrap-files-location/bootstrap-files-location.js";
-// import { findActiomanNodeModulesPaths } from "./findNodeModulesPaths.js";
 
-export async function makebootstrapHTTPListenerFileModule(cwd: URL) {
-  // const actiomanNodeModulePath: URL | null =
-  //   (await findActiomanNodeModulesPaths(cwd).next()).value ?? null;
-  // if (!actiomanNodeModulePath) throw new Error("actioman is not installed");
-  // const actionsFolder = new URL("./.cache/serve.js", actiomanNodeModulePath);
-  return getBootstrapFilesLocation();
-}
+export const makebootstrapHTTPListenerFileModule = (cwd: URL) =>
+  getBootstrapFilesLocation();
