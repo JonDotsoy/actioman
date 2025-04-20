4070a30d0682000a6a2dd88d2a8a649afbe37341
Jonathan Delgado <hi@jon.soy>
2025-04-20 00:36:25 -0400

    test: remove focus from serve command logging test

--- tests/integration/client.spec.ts
+++ tests/integration/client.spec.ts
@@ -63,7 +63,7 @@ describe("actioman serve command", () => {
     expect(stdoutText).toContain("Usage:");
   });
 
-  it.only("should execute the serve command and log hello action", async () => {
+  it("should execute the serve command and log hello action", async () => {
     const { shell, actioman } = await initializeActiomanCli();
 
     await prepareScript("test_4", "app.ts");
